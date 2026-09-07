2b88e4cb3ace90a9e729564b68628871ce7f7313
ebin6 <ebin.oneteam@gmail.com>
2026-09-07 11:46:02 +0530

    Change in email configurtion for TLS

--- prap_backend/settings.py
+++ prap_backend/settings.py
@@ -163,7 +163,7 @@ CORS_ALLOW_METHODS = [
 EMAIL_BACKEND = os.getenv('EMAIL_BACKEND', 'django.core.mail.backends.smtp.EmailBackend')
 EMAIL_HOST = os.getenv('EMAIL_HOST', 'smtp.gmail.com')
 EMAIL_PORT = int(os.getenv('EMAIL_PORT', '587'))
-EMAIL_USE_TLS = os.getenv('EMAIL_USE_TLS', 'True').lower() == 'True'
+EMAIL_USE_TLS = os.getenv('EMAIL_USE_TLS', 'True') == 'True'
 EMAIL_HOST_USER = os.getenv('EMAIL_HOST_USER', '')
 EMAIL_HOST_PASSWORD = os.getenv('EMAIL_HOST_PASSWORD', '')
 DEFAULT_FROM_EMAIL = os.getenv('DEFAULT_FROM_EMAIL', 'PRAP <oneteamprap@gmail.com>')
